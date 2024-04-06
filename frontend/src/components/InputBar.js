cff1102500832bf0071cc6aa57395642f528b6de
Mauricio Alvarez <mras21305@gmail.com>
2024-04-06 12:55:36 -0700

    crossing my fingers

--- frontend/src/components/InputBar.js
+++ frontend/src/components/InputBar.js
@@ -1,17 +1,18 @@
 import { Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
 
 const InputBar = ({ placeholder, onChange, onSubmit }) => {
   return (
     <Grid container>
       <input placeholder={placeholder} onChange={onChange} />
-      <Button onClick={onSubmit}>Submit</Button>
+      <Button
+        onClick={onSubmit}
+        startIcon={<SendIcon style={{ color: "#007bff" }} />}
+      >
+        Send
+      </Button>
     </Grid>
   );
 };
-
-// make this prettier:
-// make the submit button a send icon
-// This is the modification I did.
-
 export default InputBar;
